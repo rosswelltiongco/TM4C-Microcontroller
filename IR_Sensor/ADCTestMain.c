7857e9977a260e222c916ccae838c69d4c1966c4
Rosswell Tiongco <rosswelltiongco@gmail.com>
2018-10-31 15:29:03 -0700

    Added lookup table method for ir sensor

--- IR_Sensor/ADCTestMain.c
+++ IR_Sensor/ADCTestMain.c
@@ -37,11 +37,14 @@ void WaitForInterrupt(void);  // low power mode
 void Display_Info(volatile unsigned long cm);
 unsigned int getCm(unsigned long ADCvalue);
 unsigned int getPercent(unsigned long ADCvalue);
-
+unsigned int getLookup(unsigned long ADCvalue);
+unsigned int getAbs(int n);
+void Delay(void);
 
 volatile unsigned long ADCvalue;
 volatile unsigned long  cm;
 volatile unsigned long  pct;
+volatile unsigned long  lookupCm;
 // The digital number ADCvalue is a representation of the voltage on PE4 
 // voltage  ADCvalue
 // 0.00V     0
@@ -50,6 +53,7 @@ volatile unsigned long  pct;
 // 2.25V    3072
 // 3.00V    4095
 int main(void){unsigned long volatile delay;
+	Nokia5110_Init();
   PLL_Init();                           // 80 MHz
   ADC0_InitSWTriggerSeq3_Ch1();         // ADC initialization PE2/AIN1
   SYSCTL_RCGC2_R |= SYSCTL_RCGC2_GPIOF; // activate port F
@@ -65,8 +69,10 @@ int main(void){unsigned long volatile delay;
     ADCvalue = ADC0_InSeq3();
     GPIO_PORTF_DATA_R &= ~0x04;
 		cm = getCm(ADCvalue);
+		lookupCm = getLookup(ADCvalue);
 		pct = getPercent(ADCvalue);
-		//Display_Info(cm);
+		Display_Info(cm);
+		Delay();
   }
 }
 
@@ -83,7 +89,42 @@ unsigned int getPercent(unsigned long ADCvalue){
 	
 
 void Display_Info(volatile unsigned long cm){
+	
 	Nokia5110_Clear();
-  Nokia5110_OutString("CM:    ");
+  Nokia5110_OutString("Eqn:");
 	Nokia5110_OutUDec(cm);
+	
+	Nokia5110_SetCursor(0, 2);
+	Nokia5110_OutString("Tbl:");
+	Nokia5110_OutUDec(lookupCm);
+}
+
+unsigned int getAbs(int n) 
+{ 
+  int const mask = n >> (sizeof(int) * 8 - 1); 
+  return ((n + mask) ^ mask); 
+} 
+
+unsigned int getLookup(unsigned long ADCvalue){
+	int adcOutput[15] = {3400, 2650, 2100, 1730, 1440, 1200, 1070, 960, 840, 790, 630, 650, 530};
+	int distance[15] =  {  10,   15,   20,   25,   30,   35,   40,  45,  50,  55,  60,  65,  70};
+	unsigned int closest = getAbs(ADCvalue-adcOutput[0]);
+	unsigned int val = distance[0];
+	unsigned int i = 0;
+	
+	for (i = 0; i < 15; i++){
+		if (  (getAbs(adcOutput[i]-ADCvalue)) < closest ){
+				closest = getAbs(ADCvalue-adcOutput[i]);
+				val = distance[i];
+		}
+	}
+	return val;
+}
+	
+	
+void Delay(void){unsigned long volatile time;
+  time = 2*727240*50/91;  // 0.1sec
+  while(time){
+		time--;
+  }
 }
